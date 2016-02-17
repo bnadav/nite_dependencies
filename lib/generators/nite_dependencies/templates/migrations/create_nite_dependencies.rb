278228b154d82ded63aa62d3568d4a3b6cd16254
Nadav Blum <nadav@nite.org.il>
2016-02-17 16:47:37 +0200

    fix migration template

--- lib/generators/nite_dependencies/templates/migrations/create_nite_dependencies.rb
+++ lib/generators/nite_dependencies/templates/migrations/create_nite_dependencies.rb
@@ -1,7 +1,7 @@
 class CreateNiteDependencies < ActiveRecord::Migration
 
   def self.change
-    create_table nite_dependencies do |t|
+    create_table :nite_dependencies do |t|
       t.string :dependentable_type
 
       t.integer :dependentable_a_id;
